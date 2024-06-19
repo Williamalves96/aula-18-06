e635baade035b54014ad0526b2929ab2e2999223
William <165970735+Williamalves96@users.noreply.github.com>
2024-06-18 22:35:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+const campoLogin = document.getElementById("username");
+const campoSenha = document.getElementById("password");
+const campoNovoLogin = document.getElementById("newusername");
+const campoNovaSenha = document.getElementById("newpassword");
+const campoRepSenha = document.getElementById("reppassword");
+
+function logar(){
+    let login = campoLogin.value;
+    let senha = campoSenha.value;
+    let mensagem = "Usuário ou senha incorreta!";
+    let bancoDeDados = JSON.parse(localStorage.getItem("bancoDeDados"));
+if (bancoDeDados == null) {
+    mensagem = "Nenhum usuário cadastrado até o momento";
+} else {
+    for (let usuario of bancoDeDados) {
+        if (usuario.login == login && usuario.senha == senha) {
+            mensagem = "Parabéns, você logou!";
+            localStorage.setItem("logado", JSON.stringify(usuario));
+            window.location.href="home.html"
+            break;
+        }
+    }
+    
+}
+alert(mensagem);
+}
+function logout(){
+    window.location.href = 'index.html'
+}
+function cadastrar(){
+    if (campoNovaSenha.value == campoRepSenha.value) {
+        const usuario = {
+            login: campoNovoLogin.value,
+            senha: campoNovaSenha.value,
+        }
+        
+let bancoDeDados = JSON.parse(localStorage.getItem("bancoDeDados"));
+if (bancoDeDados == null) {
+    bancoDeDados = [];
+}
+bancoDeDados.push(usuario);
+localStorage.setItem("bancoDeDados", JSON.stringify(bancoDeDados));
+        alert("Usuário cadastrado com sucesso!");
+
+} else {
+        alert("As senhas são diferentes!");
+    }
+    
+}
+
+
+const campoTitulo = document.getElementById("namebook");
+const campoAutor = document.getElementById("autorBook");
+const campoGenero = document.getElementById("genero");
+const campoisbn= document.getElementById("isbn");
+
+function cadastroLivro(){
+
+        let titulo = campoTitulo.value;
+        let autor = campoTitulo.value;
+        let genero = campoTitulo.value;
+        let isbn = campoTitulo.value;
+        let mensagem = "Cadastro Efetuado!";
+        alert(mensagem);
+        
+
+}
